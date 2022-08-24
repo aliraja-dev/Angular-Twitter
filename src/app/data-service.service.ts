94b03dffedfcfcfe8d597f49143daa0bfde627b3
Ali Raja <aliraja.en@gmail.com>
2022-08-24 11:45:01 -0400

    Added: Delete Functionality

--- src/app/data-service.service.ts
+++ src/app/data-service.service.ts
@@ -1,18 +1,24 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, } from '@angular/common/http';
 import { Post } from './post.model';
-import { catchError, map, throwError } from 'rxjs';
+import { catchError, map, Subject, throwError } from 'rxjs';
 
-
+export interface PostResponse {
+  acknowledged: boolean;
+  insertedId: string;
+}
 @Injectable({ providedIn: 'root' })
 export class DataServiceService {
   API_URL: string = 'http://localhost:3000/';
-  headers = new HttpHeaders({
-    'Content-Type': 'application/json',
-    'Access-Control-Allow-Origin': 'localhost',
-    'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
-    'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authorization'
-  });
+
+  postsSubject = new Subject<Post[]>();
+
+  // headers = new HttpHeaders({
+  //   'Content-Type': 'application/json',
+  //   'Access-Control-Allow-Origin': 'localhost',
+  //   'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+  //   'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authorization'
+  // });
   constructor(private http: HttpClient) { }
 
   getAllPosts() {
@@ -22,6 +28,7 @@ export class DataServiceService {
         for (let post of responseData) {
           postsArray.push({ ...post, id: post["_id"]! })
         }
+        this.postsSubject.next(postsArray);
         return postsArray;
       })
     );
@@ -29,12 +36,12 @@ export class DataServiceService {
   }
 
   addPost(post: Post) {
-    return this.http.post(this.API_URL + 'record/add', post);
+    return this.http.post<PostResponse>(this.API_URL + 'record/add', post);
   }
 
-  // deleteTaskfromMongoDb(id: string) {
-  //   return this.http.delete('http://localhost:5000/' + id);
-  // }
+  deleteTask(id: string) {
+    return this.http.delete(this.API_URL + id);
+  }
   // editTaskOnMongoDb(id: string, data: Task) {
   //   return this.http.post('http://localhost:5000/update/' + id, data);
   // }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,23 +11,42 @@ import { Post } from '../post.model';
 export class HomeComponent implements OnInit {
   @ViewChild('f', { static: false }) form!: NgForm;
   posts: Post[] = [];
+
   constructor(private dataSvc: DataServiceService) { }
 
   ngOnInit(): void {
+    this.dataSvc.postsSubject.subscribe(posts => { console.log(posts); this.posts = posts; });
     this.dataSvc.getAllPosts().subscribe(res => {
-      console.log(res);
-      this.posts = res;
+      this.dataSvc.postsSubject.next(res);
     });
   }
   onSubmit() {
     console.log('submit' + this.form.value.post);
     //TODO Call the backend to save the post
-    this.posts.push({ user: "Ali", post: this.form.value.post });
+    //this.posts.push({ user: "Ali", post: this.form.value.post });
     this.dataSvc.addPost({ user: "Ali", post: this.form.value.post }).subscribe(res => {
       console.log(res);
+      if (res.acknowledged && res.insertedId !== undefined) {
+        this.posts.push({ user: "Ali", post: this.form.value.post, _id: res.insertedId });
+      }
     });
   }
+  onDelete(_id: string | null) {
+    //TODO we need to id of the post to delete
+    if (_id !== null) {
+      this.dataSvc.deleteTask(_id).subscribe(res => {
+        console.log(res);
+        //TODO remove from the array using the ID as filter
+        this.posts = this.posts.filter(post => post._id !== _id);
+      }
+      );
+    }
+  }
 
+  onEdit() {
+    //TODO we need to id of the post to edit, and then open a modal to edit it
+
+  }
   //TODO bring back the posts from backend
 
 }
